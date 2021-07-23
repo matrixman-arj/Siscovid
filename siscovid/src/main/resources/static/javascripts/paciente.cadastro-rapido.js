6b6acacf26cae7313d20677ee3d04914e0a3fed9
matri <matri@NILL-ESCRITORIO>
2021-07-23 11:29:50 -0300

    Javascript dos combos, modularizados.

--- siscovid/src/main/resources/static/javascripts/paciente.cadastro-rapido.js
+++ siscovid/src/main/resources/static/javascripts/paciente.cadastro-rapido.js
@@ -1,51 +1,56 @@
-$(function() {
+var Siscovid = Siscovid || {};
+
+	Siscovid.PessoaCadastroRapido = (function() {
+		
+		function PessoaCadastroRapido() {
+		this.modal = $('#modalCadastroRapidoPaciente');
+		this.botaoSalvar = this.modal.find('.js-modal-cadastro-pessoa-salvar-btn');
+		this.form = this.modal.find('form');
+		this.url = this.form.attr('action');
+		this.inputIdentidadePaciente = $('#identidadePaciente');
+		this.inputNomePaciente = $('#nomePaciente');
+		this.inputNomeGuerraPaciente = $('#nomeGuerraPaciente');
+		this.inputEmailPaciente = $('#emailPaciente');
+		this.selectPostoPaciente = $('#postoPaciente');
+		this.selectOmPaciente = $('#omPaciente');
+		this.selectTipoPaciente = $('#tipoPaciente');
+		this.containerMensagemErro = $('.js-mensagem-cadastro-rapido-pessoa');		
+	}
 	
-	var modal = $('#modalCadastroRapidoPaciente');
-	var botaoSalvar = modal.find('.js-modal-cadastro-pessoa-salvar-btn');
-	var form = modal.find('form');
-	form.on('submit', function(event) {event.preDefault() });
-	var url = form.attr('action');
-	var inputIdentidadePaciente = $('#identidadePaciente');
-	var inputNomePaciente = $('#nomePaciente');
-	var inputNomeGuerraPaciente = $('#nomeGuerraPaciente');
-	var inputEmailPaciente = $('#emailPaciente');
-	var selectPostoPaciente = $('#postoPaciente');
-	var selectOmPaciente = $('#omPaciente');
-	var selectTipoPaciente = $('#tipoPaciente');
-	var containerMensagemErro = $('.js-mensagem-cadastro-rapido-pessoa');
-	
-	
-	modal.on('shown.bs.modal', onModalShow);
-	modal.on('hide.bs.modal', onModalClose);
-	botaoSalvar.on('click', onBotaoSalvarClick);
+	PessoaCadastroRapido.prototype.iniciar = function() {
+		this.form.on('submit', function(event) {event.preDefault() });	
+		this.modal.on('shown.bs.modal', onModalShow.bind(this));
+		this.modal.on('hide.bs.modal', onModalClose.bind(this));
+		this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
+	}	
 	
 	function onModalShow() {
-		inputIdentidadePaciente.focus();
-		containerMensagemErro.addClass('hidden');
-		form.find('.form-group').removeClass('has-error');
+		this.inputIdentidadePaciente.focus();
 	}
 	
 	function onModalClose(){
-	 inputIdentidadePaciente.val('');
-	 inputNomePaciente.val('');
-	 inputNomeGuerraPaciente.val('');
-	 inputEmailPaciente.val('');
-	 selectPostoPaciente.val('');
-	 selectOmPaciente.val('');
-	 selectTipoPaciente.val('');
+		 this.inputIdentidadePaciente.val('');
+		 this.inputNomePaciente.val('');
+		 this.inputNomeGuerraPaciente.val('');
+		 this.inputEmailPaciente.val('');
+		 this.selectPostoPaciente.val('');
+		 this.selectOmPaciente.val('');
+		 this.selectTipoPaciente.val('');
+		 this.containerMensagemErro.addClass('hidden');
+		 this.form.find('.form-group').removeClass('has-error');
 	}
 	
 	function onBotaoSalvarClick(){
-		var identidadePaciente = inputIdentidadePaciente.val().trim();
-		var nomePaciente = inputNomePaciente.val().trim();
-		var nomeGuerraPaciente = inputNomeGuerraPaciente.val().trim();
-		var emailPaciente = inputEmailPaciente.val().trim();
-		var postoPaciente = selectPostoPaciente.val().trim();
-		var omPaciente = selectOmPaciente.val().trim();
-		var tipoPaciente = selectTipoPaciente.val().trim();
+		var identidadePaciente = this.inputIdentidadePaciente.val().trim();
+		var nomePaciente = this.inputNomePaciente.val().trim();
+		var nomeGuerraPaciente = this.inputNomeGuerraPaciente.val().trim();
+		var emailPaciente = this.inputEmailPaciente.val().trim();
+		var postoPaciente = this.selectPostoPaciente.val().trim();
+		var omPaciente = this.selectOmPaciente.val().trim();
+		var tipoPaciente = this.selectTipoPaciente.val().trim();
 		
 		$.ajax({
-			url: url,
+			url: this.url,
 			method: 'POST',
 			contentType: 'application/json',
 			data: JSON.stringify({identidade: identidadePaciente,
@@ -56,16 +61,16 @@ $(function() {
 								  om: {codigo: omPaciente},
 								  tipoPaciente: tipoPaciente 
 }),
-			error: onErroSalvandoPaciente,
-			success: onPessoaSalva
+			error: onErroSalvandoPaciente.bind(this),
+			success: onPessoaSalva.bind(this)
 		});
 	}
 	
 	function onErroSalvandoPaciente(obj){
 		var mensagemErro = obj.responseText;
-		containerMensagemErro.removeClass('hidden');
-		containerMensagemErro.html('<span>' + mensagemErro + '</span>');
-		form.find('.form-group').addClass('has-error');
+		this.containerMensagemErro.removeClass('hidden');
+		this.containerMensagemErro.html('<span>' + mensagemErro + '</span>');
+		this.form.find('.form-group').addClass('has-error');
 		
 	}
 	
@@ -73,7 +78,17 @@ $(function() {
 		var comboPaciente = $('#paciente');
 		comboPaciente.append('<option value =' + paciente.codigo + '>' + paciente.nomeGuerra + '</option>');
 		comboPaciente.val(paciente.codigo);
-		modal.modal('hide');
+		this.modal.modal('hide');
 	}
+	
+	return PessoaCadastroRapido;
+	
+}());
+
+	
+$(function() {
+	
+	var pessoaCadastroRapido = new Siscovid.PessoaCadastroRapido();
+	pessoaCadastroRapido.iniciar();
 		
 });
